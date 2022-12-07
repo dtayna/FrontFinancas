a01f7cd960a71c580962150f7af8be55a4825eb4
Ana Luisa <53523588+anaestevam@users.noreply.github.com>
2022-12-06 21:21:01 -0300

    a

--- src/server/index.js
+++ src/server/index.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 const baseURL = 'http://localhost:8080/api';
 
-const token = localStorage.getItem('auth-token')
+//const token = localStorage.getItem('auth-token')
 
 export const api = axios.create({
   baseURL: `${baseURL}`,
   headers: {
    'Content-Type': 'application/json',
-   'Authorization': `Bearer ${token}`,
+  // 'Authorization': `Bearer ${token}`,
   },
 });
  
